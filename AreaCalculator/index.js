1c1863f3b8b41cd2e89d8888748bd12aed3ad4ad
Maharun Nessa Meem <maharunnessameem86@gmail.com>
2023-08-27 15:09:11 +0600

    added area calculator

---
+++ AreaCalculator/index.js
@@ -0,0 +1,39 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+const bodyParser = require('body-parser');
+
+app.use(bodyParser.urlencoded({ extended: false }))
+
+// parse application/json
+app.use(bodyParser.json())
+
+
+app.get("/", (req, res) => {
+    res.sendFile(__dirname + "/index.html");
+})
+
+app.get("/circle", (req, res) => {
+    res.sendFile(__dirname + "/circle.html");
+})
+
+app.post("/circle", (req, res) => {
+    const radius = req.body.radius;
+    const area = Math.PI * radius * radius;
+    res.send(`Area of Circle is :${area}`);
+})
+
+app.get("/triangle", (req, res) => {
+    res.sendFile(__dirname + "/triangle.html")
+})
+
+app.post("/triangle", (req, res) => {
+    const height = req.body.height;
+    const base = req.body.base;
+    const area = 0.5 * base * height;
+    res.send(`<h2>Area of Triangle is :${area} </h2>`)
+})
+
+app.listen(PORT, () => {
+    console.log(`Sever is running at http://localhost:${PORT}`);
+})
